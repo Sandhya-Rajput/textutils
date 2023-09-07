3a7cfac6a7abbcb59300f758708f90e7e88921ee
Sandhya-Rajput <67312144+Sandhya-Rajput@users.noreply.github.com>
2023-09-07 17:04:34 +0530

    Update TextForm.js

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -66,24 +66,24 @@ export default function TextForm(props) {
     
     <div className="container" style={{color:props.mode==='dark'?'white':'#061c31'}}>
         
-        <h1 >{props.heading} </h1>
+        <h1 className='m-2'>{props.heading} </h1>
      
 <div className="mb-3">
   <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor:props.mode==='dark'?'grey':'white',
 color:props.mode==='dark'?'white':'#061c31'  }} id="myBox" rows="10"></textarea>
 </div>
-<button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert To Uppercase</button>
-<button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert To Lowercase</button>
-<button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear Text</button>
-<button className="btn btn-primary mx-2" onClick={handleCopy}>Copy Text</button>
-<button className="btn btn-primary mx-2" onClick={handleExtraSpaces}>Remove Extra Space Text</button>
+<button className="btn btn-primary mx-2 my-2" onClick={handleUpClick}>Convert To Uppercase</button>
+<button className="btn btn-primary mx-2 my-2" onClick={handleLoClick}>Convert To Lowercase</button>
+<button className="btn btn-primary mx-2 my-2" onClick={handleClearClick}>Clear Text</button>
+<button className="btn btn-primary mx-2 my-2" onClick={handleCopy}>Copy Text</button>
+<button className="btn btn-primary mx-2 my-2" onClick={handleExtraSpaces}>Remove Extra Space Text</button>
 
     </div>
     <div className="container my-3" style={{color:props.mode==='dark'?'white':'##061c31'}}>
     <div className="container my-3" style={{color:props.mode==='light'?'black':'white '}}>
         <h2>Your Text summary</h2>
-        <p>{text.split(" ").length}, {text.length} characters</p>
-        <p>{0.008* text.split(" ").length}Minutes Read</p>
+        <p>{text.split((/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+        <p>{0.008* text.split(" ").filter((element)=>{return element.length!==0}).length}Minutes Read</p>
         <h2>Preview</h2>
         <p>{text.length>0?text:"Enter something in the textbox above to preview it here"}</p>
     </div>
